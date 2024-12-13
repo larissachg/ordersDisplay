78a3dcae6292be2721325db2e216798b4e6f7c7d
soljancic <alejandrosoljancic@gmail.com>
2024-12-13 16:34:47 -0400

    Visitas.Identificador

--- src/actions/getHistory.ts
+++ src/actions/getHistory.ts
@@ -9,52 +9,18 @@ export async function getHistoryDb(nombreEquipo: string): Promise<OrdenDb[]> {
       .startOf('day')
       .format('YYYY-MM-DD HH:mm:ss')
 
+    let despachoStr = ` INNER JOIN TiposProductos ON TiposProductos.TipoProductoID = Productos.TipoProductoID 
+        INNER JOIN Impresoras ON TiposProductos.kitchenDisplayID = Impresoras.ImpresoraID  AND Impresoras.NombreFisico LIKE '%${nombreEquipo}%' `
+
     if (nombreEquipo === 'DespachoToptech') {
-      const pool = await poolPromise
-      const result = await pool.request().query(`
-     SELECT  
-      Visitas.Id AS id,
-      Mesas.Nombre AS mesa,
-      Meseros.Nombre AS mesero,
-      TipoEnvios.Nombre AS tipoEnvio,
-      ParaLlevar.Nombre AS paraLlevar, 
-      Productos.Nombre AS producto,
-      DetalleCuenta.Orden AS orden,
-      DetalleCuenta.Cantidad AS cantidad,
-      CAST(DetalleCuenta.Hora AS DATETIME) AS hora,
-      DetalleCuenta.Borrada AS borrada, 
-      Observaciones.Observacion AS observacion,
-      (
-          SELECT STRING_AGG(P2.Nombre, ',') 
-          FROM ProductosCombos
-          INNER JOIN Productos AS P2 ON P2.ID = ProductosCombos.ProductoID 
-          WHERE ProductosCombos.DetalleCuentaID = DetalleCuenta.ID 
-      ) AS productosCombo
-  FROM 
-      DetalleCuenta INNER JOIN 
-      Visitas ON DetalleCuenta.visitaID = Visitas.ID 
-      INNER JOIN Productos ON Productos.ID = DetalleCuenta.ProductoID 
-      INNER JOIN Meseros ON Meseros.MeseroID = DetalleCuenta.TomoPedidoMeseroID 
-      LEFT JOIN Observaciones ON Observaciones.DetalleCuentaID = DetalleCuenta.ID 
-      LEFT JOIN Mesas ON Mesas.ID = Visitas.MesaID 
-      LEFT JOIN TipoEnvios ON Visitas.TipoEnvioID = TipoEnvios.TipoEnvioID 
-      LEFT JOIN ParaLlevar ON Visitas.ParaLlevarID = ParaLlevar.ParaLlevarID      
-  WHERE 
-      DetalleCuenta.Hora >= '${startOfToday}'
-      AND DetalleCuenta.Terminado IS NOT NULL   
-  ORDER BY 
-      DetalleCuenta.Orden desc, 
-      Visitas.Id desc, 
-      DetalleCuenta.Hora, 
-      Productos.Nombre;
-    `)
-      return result.recordset as OrdenDb[]
-    } else {
-      const pool = await poolPromise
-      const result = await pool.request().query(`
+      despachoStr = ``
+    }
+
+    const pool = await poolPromise
+    const result = await pool.request().query(`
        SELECT  
         Visitas.Id AS id,
-        Mesas.Nombre AS mesa,
+        iif(Visitas.Identificador is null OR LEN(TRIM(Visitas.Identificador)) <= 2, Mesas.Nombre,Visitas.Identificador) AS mesa,
         Meseros.Nombre AS mesero,
         TipoEnvios.Nombre AS tipoEnvio,
         ParaLlevar.Nombre AS paraLlevar, 
@@ -79,8 +45,7 @@ export async function getHistoryDb(nombreEquipo: string): Promise<OrdenDb[]> {
         LEFT JOIN Mesas ON Mesas.ID = Visitas.MesaID 
         LEFT JOIN TipoEnvios ON Visitas.TipoEnvioID = TipoEnvios.TipoEnvioID 
         LEFT JOIN ParaLlevar ON Visitas.ParaLlevarID = ParaLlevar.ParaLlevarID 
-        INNER JOIN TiposProductos ON TiposProductos.TipoProductoID = Productos.TipoProductoID 
-        INNER JOIN Impresoras ON TiposProductos.kitchenDisplayID = Impresoras.ImpresoraID  AND Impresoras.NombreFisico LIKE '%${nombreEquipo}%'
+        ${despachoStr}       
     WHERE 
         DetalleCuenta.Hora >= '${startOfToday}'
         AND DetalleCuenta.Terminado IS NOT NULL   
@@ -90,8 +55,7 @@ export async function getHistoryDb(nombreEquipo: string): Promise<OrdenDb[]> {
         DetalleCuenta.Hora, 
         Productos.Nombre;
       `)
-      return result.recordset as OrdenDb[]
-    }
+    return result.recordset as OrdenDb[]
   } catch (error) {
     console.error('Error al obtener las órdenes:', error)
     throw new Error('No se pudieron obtener las órdenes')
--- src/actions/getOrdenes.ts
+++ src/actions/getOrdenes.ts
@@ -9,64 +9,15 @@ export async function getOrdenesDb(nombreEquipo: string): Promise<OrdenDb[]> {
       .startOf('day')
       .format('YYYY-MM-DD HH:mm:ss')
 
+    let despachoStr = ` INNER JOIN TiposProductos ON TiposProductos.TipoProductoID = Productos.TipoProductoID 
+      INNER JOIN Impresoras ON TiposProductos.kitchenDisplayID = Impresoras.ImpresoraID  AND Impresoras.NombreFisico LIKE '%${nombreEquipo}%' `
+
     if (nombreEquipo === 'DespachoToptech') {
-      const pool = await poolPromise
-      const result = await pool.request().query(`
-  WITH TopVisitas AS (
-      SELECT 
-          DetalleCuenta.VisitaID,DetalleCuenta.Orden,
-          ROW_NUMBER() OVER (ORDER BY DetalleCuenta.Orden, DetalleCuenta.VisitaID) AS RN
-      FROM 
-          DetalleCuenta
-      INNER JOIN Productos ON Productos.ID = DetalleCuenta.ProductoID      
-      WHERE 
-          DetalleCuenta.Hora >= '${startOfToday}'
-          AND DetalleCuenta.Terminado IS NULL
-      GROUP BY 
-          DetalleCuenta.VisitaID, DetalleCuenta.Orden
-  )
-  SELECT  
-      Visitas.Id AS id,
-      Mesas.Nombre AS mesa,
-      Meseros.Nombre AS mesero,
-      TipoEnvios.Nombre AS tipoEnvio,
-      ParaLlevar.Nombre AS paraLlevar, 
-      Productos.Nombre AS producto,
-      DetalleCuenta.Orden AS orden,
-      DetalleCuenta.Cantidad AS cantidad,
-      CAST(DetalleCuenta.Hora AS DATETIME) AS hora,
-      DetalleCuenta.Borrada AS borrada, 
-      Observaciones.Observacion AS observacion,
-      (
-          SELECT STRING_AGG(P2.Nombre, ',') 
-          FROM ProductosCombos
-          INNER JOIN Productos AS P2 ON P2.ID = ProductosCombos.ProductoID 
-          WHERE ProductosCombos.DetalleCuentaID = DetalleCuenta.ID 
-      ) AS productosCombo
-  FROM 
-      DetalleCuenta INNER JOIN 
-      Visitas ON DetalleCuenta.visitaID = Visitas.ID 
-      INNER JOIN Productos ON Productos.ID = DetalleCuenta.ProductoID 
-      INNER JOIN Meseros ON Meseros.MeseroID = DetalleCuenta.TomoPedidoMeseroID 
-      LEFT JOIN Observaciones ON Observaciones.DetalleCuentaID = DetalleCuenta.ID 
-      LEFT JOIN Mesas ON Mesas.ID = Visitas.MesaID 
-      LEFT JOIN TipoEnvios ON Visitas.TipoEnvioID = TipoEnvios.TipoEnvioID 
-      LEFT JOIN ParaLlevar ON Visitas.ParaLlevarID = ParaLlevar.ParaLlevarID       
-      INNER JOIN TopVisitas ON DetalleCuenta.VisitaID = TopVisitas.VisitaID and DetalleCuenta.Orden = TopVisitas.Orden
-  WHERE 
-      DetalleCuenta.Hora >= '${startOfToday}'
-      AND DetalleCuenta.Terminado IS NULL
-      AND TopVisitas.RN <= 9
-  ORDER BY 
-      DetalleCuenta.Orden, 
-      Visitas.Id, 
-      DetalleCuenta.Hora, 
-      Productos.Nombre;
-  `)
-      return result.recordset as OrdenDb[]
-    } else {
-      const pool = await poolPromise
-      const result = await pool.request().query(`
+      despachoStr = ''
+    }
+    console.log(nombreEquipo)
+
+    const query1 = `
   WITH TopVisitas AS (
       SELECT 
           DetalleCuenta.VisitaID,DetalleCuenta.Orden,
@@ -74,18 +25,16 @@ export async function getOrdenesDb(nombreEquipo: string): Promise<OrdenDb[]> {
       FROM 
           DetalleCuenta
       INNER JOIN Productos ON Productos.ID = DetalleCuenta.ProductoID
-      INNER JOIN TiposProductos ON TiposProductos.TipoProductoID = Productos.TipoProductoID
-      INNER JOIN Impresoras ON TiposProductos.kitchenDisplayID = Impresoras.ImpresoraID
+      ${despachoStr}
       WHERE 
           DetalleCuenta.Hora >= '${startOfToday}'
-          AND DetalleCuenta.Terminado IS NULL
-          AND Impresoras.NombreFisico LIKE '%${nombreEquipo}%'
+          AND DetalleCuenta.Terminado IS NULL      
       GROUP BY 
           DetalleCuenta.VisitaID, DetalleCuenta.Orden
   )
   SELECT  
       Visitas.Id AS id,
-      Mesas.Nombre AS mesa,
+      iif(Visitas.Identificador is null OR LEN(TRIM(Visitas.Identificador)) <= 2, Mesas.Nombre,Visitas.Identificador) AS mesa,
       Meseros.Nombre AS mesero,
       TipoEnvios.Nombre AS tipoEnvio,
       ParaLlevar.Nombre AS paraLlevar, 
@@ -110,9 +59,8 @@ export async function getOrdenesDb(nombreEquipo: string): Promise<OrdenDb[]> {
       LEFT JOIN Mesas ON Mesas.ID = Visitas.MesaID 
       LEFT JOIN TipoEnvios ON Visitas.TipoEnvioID = TipoEnvios.TipoEnvioID 
       LEFT JOIN ParaLlevar ON Visitas.ParaLlevarID = ParaLlevar.ParaLlevarID 
-      INNER JOIN TiposProductos ON TiposProductos.TipoProductoID = Productos.TipoProductoID 
-      INNER JOIN Impresoras ON TiposProductos.kitchenDisplayID = Impresoras.ImpresoraID  AND Impresoras.NombreFisico LIKE '%${nombreEquipo}%'
       INNER JOIN TopVisitas ON DetalleCuenta.VisitaID = TopVisitas.VisitaID and DetalleCuenta.Orden = TopVisitas.Orden
+      ${despachoStr}      
   WHERE 
       DetalleCuenta.Hora >= '${startOfToday}'
       AND DetalleCuenta.Terminado IS NULL
@@ -122,9 +70,10 @@ export async function getOrdenesDb(nombreEquipo: string): Promise<OrdenDb[]> {
       Visitas.Id, 
       DetalleCuenta.Hora, 
       Productos.Nombre;
-  `)
-      return result.recordset as OrdenDb[]
-    }
+  `
+    const pool = await poolPromise
+    const result = await pool.request().query(query1)
+    return result.recordset as OrdenDb[]
   } catch (error) {
     console.error('Error al obtener las órdenes:', error)
     throw new Error('No se pudieron obtener las órdenes')
