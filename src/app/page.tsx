4e68350bf0ab0e30d1bd3e9b03ed7e8da382e80c
Larissa <larissachg@gmail.com>
2024-12-14 14:59:10 -0400

    fixed character

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { OrdersPage } from "@/app/(pages)/(orders)/(components)/Orders";
 export default function Home() {
   return (
     <>
-      <OrdersPage />;
+      <OrdersPage />
     </>
   );
 }
