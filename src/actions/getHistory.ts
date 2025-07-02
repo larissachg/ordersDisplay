2050fe72cea765259b0bf077e131f03b8b1c42c2
Soljancic <alejandrosoljancic@gmail.com>
2025-07-02 17:23:32 -0400

    Query get History mas eficiente y rapido

--- src/actions/getHistory.ts
+++ src/actions/getHistory.ts
@@ -26,45 +26,91 @@ export async function getHistoryDb(nombreEquipo: string): Promise<OrdenDb[]> {
       whereStr = ' and Visitas.MesaID is not null'
     }
 
+
+    //const queryOLD =`
+    //   SELECT  
+    //    Visitas.Id AS id,
+    //    iif(Visitas.Identificador is null OR LEN(TRIM(Visitas.Identificador)) <= 2, Mesas.Nombre,Visitas.Identificador) AS mesa,
+    //    Meseros.Nombre AS mesero,
+    //    TipoEnvios.Nombre AS tipoEnvio,
+    //    ParaLlevar.Nombre AS paraLlevar, 
+    //    Productos.Nombre AS producto,
+    //    DetalleCuenta.Orden AS orden,
+    //    DetalleCuenta.Cantidad AS cantidad,
+    //    CAST(DetalleCuenta.Hora AS DATETIME) AS hora,
+    //    DetalleCuenta.Borrada AS borrada, 
+    //    Observaciones.Observacion AS observacion,
+    //    (
+    //        SELECT STRING_AGG(P2.Nombre, ',') 
+    //        FROM ProductosCombos
+    //        INNER JOIN Productos AS P2 ON P2.ID = ProductosCombos.ProductoID 
+    //        WHERE ProductosCombos.DetalleCuentaID = DetalleCuenta.ID 
+    //    ) AS productosCombo
+    //FROM 
+    //    DetalleCuenta INNER JOIN 
+    //    Visitas ON DetalleCuenta.visitaID = Visitas.ID 
+    //    INNER JOIN Productos ON Productos.ID = DetalleCuenta.ProductoID 
+    //    INNER JOIN Meseros ON Meseros.MeseroID = DetalleCuenta.TomoPedidoMeseroID 
+    //    LEFT JOIN Observaciones ON Observaciones.DetalleCuentaID = DetalleCuenta.ID 
+    //    LEFT JOIN Mesas ON Mesas.ID = Visitas.MesaID 
+    //    LEFT JOIN TipoEnvios ON Visitas.TipoEnvioID = TipoEnvios.TipoEnvioID 
+    //    LEFT JOIN ParaLlevar ON Visitas.ParaLlevarID = ParaLlevar.ParaLlevarID 
+    //    ${despachoStr}       
+    //WHERE 
+    //    iif(ParaLlevar.HoraRecoger is null, DetalleCuenta.Hora, ParaLlevar.HoraRecoger)  between '${startOfToday}' and '${startOfTomorrow}'
+    //    AND DetalleCuenta.Terminado IS NOT NULL ${whereStr}       
+    //ORDER BY 
+    //    DetalleCuenta.Orden desc, 
+    //    Visitas.Id desc, 
+    //    DetalleCuenta.Hora, 
+    //    Productos.Nombre;
+    //  `
+
+      const query1 =`
+      WITH FilteredDetalleCuenta AS (
+        SELECT ID, VisitaID, ProductoID, TomoPedidoMeseroID, Orden, Cantidad, Hora, Borrada, Terminado
+        FROM DetalleCuenta
+        WHERE Terminado IS NOT NULL
+        ),
+        ProductosCombo AS (
+          SELECT DetalleCuentaID, STRING_AGG(P2.Nombre, ',') AS productosCombo
+          FROM ProductosCombos
+          INNER JOIN Productos AS P2 ON P2.ID = ProductosCombos.ProductoID
+          GROUP BY DetalleCuentaID
+        )
+        SELECT
+          Visitas.Id AS id,
+          COALESCE(NULLIF(TRIM(Visitas.Identificador), ''), Mesas.Nombre) AS mesa,
+          Meseros.Nombre AS mesero,
+          TipoEnvios.Nombre AS tipoEnvio,
+          ParaLlevar.Nombre AS paraLlevar,
+          Productos.Nombre AS producto,
+          DetalleCuenta.Orden AS orden,
+          DetalleCuenta.Cantidad AS cantidad,
+          CAST(DetalleCuenta.Hora AS DATETIME) AS hora,
+          DetalleCuenta.Borrada AS borrada,
+          Observaciones.Observacion AS observacion,
+          PC.productosCombo
+        FROM FilteredDetalleCuenta AS DetalleCuenta
+        INNER JOIN Visitas ON DetalleCuenta.VisitaID = Visitas.ID
+        INNER JOIN Productos ON Productos.ID = DetalleCuenta.ProductoID
+        INNER JOIN Meseros ON Meseros.MeseroID = DetalleCuenta.TomoPedidoMeseroID
+        LEFT JOIN Observaciones ON Observaciones.DetalleCuentaID = DetalleCuenta.ID
+        LEFT JOIN Mesas ON Mesas.ID = Visitas.MesaID
+        LEFT JOIN TipoEnvios ON Visitas.TipoEnvioID = TipoEnvios.TipoEnvioID
+        LEFT JOIN ParaLlevar ON Visitas.ParaLlevarID = ParaLlevar.ParaLlevarID
+        ${despachoStr}   
+        LEFT JOIN ProductosCombo PC ON PC.DetalleCuentaID = DetalleCuenta.ID
+        WHERE (
+            (ParaLlevar.HoraRecoger IS NULL AND DetalleCuenta.Hora between '${startOfToday}' and '${startOfTomorrow}')
+            OR (ParaLlevar.HoraRecoger IS NOT NULL AND ParaLlevar.HoraRecoger BETWEEN '${startOfToday}' and '${startOfTomorrow}')
+        ) ${whereStr}   
+        ORDER BY DetalleCuenta.Orden DESC, Visitas.Id DESC, DetalleCuenta.Hora, Productos.Nombre;
+      `
+    //console.log('Query:', query1)
     const pool = await poolPromise
-    const result = await pool.request().query(`
-       SELECT  
-        Visitas.Id AS id,
-        iif(Visitas.Identificador is null OR LEN(TRIM(Visitas.Identificador)) <= 2, Mesas.Nombre,Visitas.Identificador) AS mesa,
-        Meseros.Nombre AS mesero,
-        TipoEnvios.Nombre AS tipoEnvio,
-        ParaLlevar.Nombre AS paraLlevar, 
-        Productos.Nombre AS producto,
-        DetalleCuenta.Orden AS orden,
-        DetalleCuenta.Cantidad AS cantidad,
-        CAST(DetalleCuenta.Hora AS DATETIME) AS hora,
-        DetalleCuenta.Borrada AS borrada, 
-        Observaciones.Observacion AS observacion,
-        (
-            SELECT STRING_AGG(P2.Nombre, ',') 
-            FROM ProductosCombos
-            INNER JOIN Productos AS P2 ON P2.ID = ProductosCombos.ProductoID 
-            WHERE ProductosCombos.DetalleCuentaID = DetalleCuenta.ID 
-        ) AS productosCombo
-    FROM 
-        DetalleCuenta INNER JOIN 
-        Visitas ON DetalleCuenta.visitaID = Visitas.ID 
-        INNER JOIN Productos ON Productos.ID = DetalleCuenta.ProductoID 
-        INNER JOIN Meseros ON Meseros.MeseroID = DetalleCuenta.TomoPedidoMeseroID 
-        LEFT JOIN Observaciones ON Observaciones.DetalleCuentaID = DetalleCuenta.ID 
-        LEFT JOIN Mesas ON Mesas.ID = Visitas.MesaID 
-        LEFT JOIN TipoEnvios ON Visitas.TipoEnvioID = TipoEnvios.TipoEnvioID 
-        LEFT JOIN ParaLlevar ON Visitas.ParaLlevarID = ParaLlevar.ParaLlevarID 
-        ${despachoStr}       
-    WHERE 
-        iif(ParaLlevar.HoraRecoger is null, DetalleCuenta.Hora, ParaLlevar.HoraRecoger)  between '${startOfToday}' and '${startOfTomorrow}'
-        AND DetalleCuenta.Terminado IS NOT NULL ${whereStr}       
-    ORDER BY 
-        DetalleCuenta.Orden desc, 
-        Visitas.Id desc, 
-        DetalleCuenta.Hora, 
-        Productos.Nombre;
-      `)
+    const result = await pool.request().query(query1)
+    
     return result.recordset as OrdenDb[]
   } catch (error) {
     console.error('Error al obtener las órdenes:', error)
